693c25b28a7a980edfb5750c23fc057418755523
Carlos Eduardo Ferreira Carrinho <carrinhoedu@gmail.com>
2022-01-25 14:49:16 -0300

    refactor: use BlogService to fetch articles from api

--- src/bff/application/commands/get-articles-page.ts
+++ src/bff/application/commands/get-articles-page.ts
@@ -1,4 +1,6 @@
 import { Request, Response } from "express";
+import { BlogService } from "../../infrastructure/services/blog-service/blog-service";
+import { AxiosAdapter } from "../../infrastructure/services/http-service/axios-adapter";
 import { Command } from "../contracts/command";
 
 export class GetArticlesPage implements Command {
@@ -8,16 +10,16 @@ export class GetArticlesPage implements Command {
       metadata: { pageTitle: "Articles" },
     };
 
-    // const articleRepository = new ArticleRepository();
-    // const articles = await articleRepository.find();
+    const httpService = new BlogService(new AxiosAdapter());
+    const articles = await httpService.getArticles();
 
-    // if (articles.isError) {
-    //   return res.render(templatePath, { ...data, articles: {} });
-    // }
+    if (!articles.length) {
+      return res.render(templatePath, { ...data, articles: {} });
+    }
 
     return res.render(templatePath, {
       ...data,
-      articles: {},
+      articles: articles,
     });
   }
 }
